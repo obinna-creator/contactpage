be7a7a1dca6170afbbe9b63e429c59f210ffaca8
obinna-creator <obinnpatrick301@gmail.com>
2024-03-19 14:59:23 +0100

    second  commit

--- contactServer.js
+++ contactServer.js
@@ -1,11 +1,11 @@
 const express= require("express")
-const contactroutes= require("./contactRouter/contactroutes")
+const contactroutes= require ("./contactRouter/contactroutes")
 const cors = require("cors")
 require('./config')
 const app = express()
 
 const port = 5000
-app.use(cors({origin:'*'}));
+app.use(cors ({origin:'*'}));
 
 // app.use(cors({origin:'*'}))
 app.use(express.json())
